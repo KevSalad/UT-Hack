b060aa9830f183ece2da64eed40b77e53de0affc
KevSalad <112964203+KevSalad@users.noreply.github.com>
2023-10-21 16:51:55 -0500

    Starter React Code

---
+++ financial-lit/src/components/Footer.js
@@ -0,0 +1,12 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 My Website</p>
+    </footer>
+  );
+}
+
+export default Footer;
